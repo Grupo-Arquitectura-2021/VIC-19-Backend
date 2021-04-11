813a201d46137c42bd30a7e645632a104b251deb
Marioly V <mariolyv7@gmail.com>
2021-04-11 12:36:13 -0400

    Agregando notacion Scheduled para programar tareas. Agregando interfaz Dao con notacion Mapper para guardar los datos obtenidos en CovidDataUpdateUtil en la base de datos

--- src/main/java/bo/ucb/edu/ingsoft/IngsoftApplication.java
+++ src/main/java/bo/ucb/edu/ingsoft/IngsoftApplication.java
@@ -1,21 +1,56 @@
 package bo.ucb.edu.ingsoft;
+import bo.ucb.edu.ingsoft.dao.CovidDataUpdateDao;
+import bo.ucb.edu.ingsoft.model.CovidData;
+import bo.ucb.edu.ingsoft.model.Transaction;
+import bo.ucb.edu.ingsoft.util.CovidDataUpdateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableScheduling
 @SpringBootApplication
-public class IngsoftApplication {
+public class IngsoftApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(IngsoftApplication.class, args);
 	}
+}
 
+@Configuration
+class SchedulerConfig implements SchedulingConfigurer {
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.initialize();
+
+		scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
 }

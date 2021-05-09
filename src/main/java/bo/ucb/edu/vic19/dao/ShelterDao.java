37be077065678253a1d3112a9989dd9cdf772c9e
Marioly <48572648+marioly7@users.noreply.github.com>
2021-05-09 18:10:45 -0400

    Creando interfaz ShelterDao y creando archivo ShelterDao.xml para la manipulacion de datos de la Base de Datos

---
+++ src/main/java/bo/ucb/edu/vic19/dao/ShelterDao.java
@@ -0,0 +1,24 @@
+package bo.ucb.edu.vic19.dao;
+
+import bo.ucb.edu.vic19.dto.LocationResponse;
+import bo.ucb.edu.vic19.dto.ShelterRequest;
+import bo.ucb.edu.vic19.model.Shelter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ShelterDao {
+    public List<LocationResponse> getShelterLocation(Integer shelterId);
+    public List<LocationResponse> getShelterLocationsByCityId(Integer cityId);
+
+    public void insertShelter(Shelter shelter);
+
+    public void deleteShelter(Shelter shelter);
+
+    public void updateShelter(Shelter shelter);
+
+    List<LocationResponse> getShelterLocations();
+
+    List<ShelterRequest> getShelterAllInfo();
+}
